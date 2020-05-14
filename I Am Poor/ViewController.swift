c41ae0130df4cefcaffb27daee1ee73ec1a8f5eb
Mostafizur Rahman <m_rahman_hridoy@yahoo.com>
2020-05-14 02:17:12 -0400

    First Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Mostafizur Rahman on 2020-05-14.
+//  Copyright © 2020 Mostafizur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
